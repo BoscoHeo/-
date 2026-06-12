250ef6f93c0084f83c27e31214e6e0ebf740effa
BoscoHeo <heoalchan@gmail.com>
2026-06-12 10:35:04 +0900

    chore(security): restrict CSP script-src
    Remove 'unsafe-inline' and 'unsafe-eval' from the Content Security Policy to improve application security and mitigate XSS risks.

--- server.ts
+++ server.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 
 // Set Security Headers Middleware for security auditor compliance
 app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com data:; img-src 'self' data: https:; connect-src 'self' https: wss:; frame-ancestors 'self' https://*.google.com https://*.googleusercontent.com;");
+  res.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com data:; img-src 'self' data: https:; connect-src 'self' https: wss:; frame-ancestors 'self' https://*.google.com https://*.googleusercontent.com;");
   res.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
   res.setHeader("X-Frame-Options", "SAMEORIGIN");
   res.setHeader("X-Content-Type-Options", "nosniff");
